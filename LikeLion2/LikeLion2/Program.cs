c7508840f3be43f527a566183fab502b940f22bf
JYYoon9410 <akaze9410@gmail.com>
2025-02-21 10:22:54 +0900

    Study2

---
+++ LikeLion2/LikeLion2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LikeLion2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int age; // 정수형 변수 age 선언
+            age = 25; //변수에 값 저장
+
+            Console.WriteLine(age); // 변수(age)에 저장된 값을 출력
+            // WriteLine 은 엔터까지, 엔터 없이는 그냥 Write만 사용
+            Console.Write(age);
+        }
+    }
+}
