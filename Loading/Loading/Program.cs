3e448f4309362839ab45bb2e5b94a321b54c7bbd
JYYoon9410 <akaze9410@gmail.com>
2025-02-21 15:04:20 +0900

    aasd

---
+++ Loading/Loading/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace Loading
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("□□□□□□□□□□");
+            Thread.Sleep(1000); //1000 이 1초다(1000밀리초)
+            Console.Clear(); // 콘솔화면 지우기
+            Console.WriteLine("■□□□□□□□□□");
+            Thread.Sleep(3000); //1000 이 1초다(1000밀리초)
+            Console.Clear(); // 콘솔화면 지우기
+            Console.WriteLine("■■□□□□□□□□");
+            Thread.Sleep(1000); //1000 이 1초다(1000밀리초)
+            Console.Clear(); // 콘솔화면 지우기
+            Console.WriteLine("■■■□□□□□□□");
+            Thread.Sleep(3000); //1000 이 1초다(1000밀리초)
+            Console.Clear(); // 콘솔화면 지우기
+            Console.WriteLine("■■■■□□□□□□");
+            Thread.Sleep(1000); //1000 이 1초다(1000밀리초)
+            Console.Clear(); // 콘솔화면 지우기
+            Console.WriteLine("■■■■■□□□□□");
+            Thread.Sleep(2000); //1000 이 1초다(1000밀리초)
+            Console.Clear(); // 콘솔화면 지우기
+            Console.WriteLine("■■■■■■□□□□");
+            Thread.Sleep(1000); //1000 이 1초다(1000밀리초)
+            Console.Clear(); // 콘솔화면 지우기
+            Console.WriteLine("■■■■■■■□□□");
+            Thread.Sleep(1000); //1000 이 1초다(1000밀리초)
+            Console.Clear(); // 콘솔화면 지우기
+            Console.WriteLine("■■■■■■■■□□");
+            Thread.Sleep(1000); //1000 이 1초다(1000밀리초)
+            Console.Clear(); // 콘솔화면 지우기
+            Console.WriteLine("■■■■■■■■■□");
+            Thread.Sleep(1000); //1000 이 1초다(1000밀리초)
+            Console.Clear(); // 콘솔화면 지우기
+            Console.WriteLine("■■■■■■■■■■");
+            Thread.Sleep(1000); //1000 이 1초다(1000밀리초)
+            Console.Clear(); // 콘솔화면 지우기
+            Console.WriteLine("텍스트 엘든링");
+
+            Console.WriteLine("스토리1");
+            Console.Clear();
+            Console.WriteLine("스토리2");
+
+
+
+
+        }
+    }
+}
