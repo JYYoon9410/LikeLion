e8dd002e6d6cef536162f35928b3ba031efda359
JYYoon9410 <akaze9410@gmail.com>
2025-02-21 17:50:05 +0900

    asdf

---
+++ TodayTask1/TodayTask1/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TodayTask1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //로딩바 시작화면
+            //게임스토리 1
+
+            Console.WriteLine("□□□□□□□□□□");
+            Thread.Sleep(1000); //1000 이 1초다(1000밀리초)
+            Console.Clear(); // 콘솔화면 지우기
+            Console.WriteLine("■□□□□□□□□□");
+            Thread.Sleep(3000); 
+            Console.Clear();
+            Console.WriteLine("■■□□□□□□□□");
+            Thread.Sleep(1000);
+            Console.Clear();
+            Console.WriteLine("■■■□□□□□□□");
+            Thread.Sleep(2000);
+            Console.Clear(); 
+            Console.WriteLine("■■■■□□□□□□");
+            Thread.Sleep(1000);
+            Console.Clear(); 
+            Console.WriteLine("■■■■■□□□□□");
+            Thread.Sleep(1000);
+            Console.Clear(); 
+            Console.WriteLine("■■■■■■□□□□");
+            Thread.Sleep(1000);
+            Console.Clear();
+            Console.WriteLine("■■■■■■■□□□");
+            Thread.Sleep(1000);
+            Console.Clear(); 
+            Console.WriteLine("■■■■■■■■□□");
+            Thread.Sleep(1000);
+            Console.Clear();
+            Console.WriteLine("■■■■■■■■■□");
+            Thread.Sleep(2000); 
+            Console.Clear();
+            Console.WriteLine("■■■■■■■■■■");
+            Thread.Sleep(1000);
+            Console.Clear();
+
+
+
+            Console.WriteLine("이곳은 필리디 대륙");
+            Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine("아름답고 비옥한 대지가 있는 이 대륙에는 예로부터 두 왕국이 있었다");
+            Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine("하나는 열정있고 활기찬 멋쟁이 사자왕국과,");
+            Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine("다른 하나는 비열하고 나태한 못난이 재규어 제국 이다.");
+            Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine("평화롭던 멋쟁이 사자왕국의 비극은 한순간에 일어났다");
+            Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine("바로 못난이 재규어 제국의 기습공격에 아름답고 활기찬 사자왕국은 혼란에 빠졌고");
+            Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine("멋쟁이 사자왕국의 국왕은 못난이 재규어 제국에 납치되고 말았다");
+            Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine("이것은 멋쟁이 사자왕국의 수호기사였던 당신의...");
+            Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine("모든것을 잃고, 더렵혀진 명예를 회복하기 위해 일어서는 이야기");
+            Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine("멋쟁이 사자 사가");
+            Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine("Press Enter to Play!");
+            Console.ReadLine();
+            Console.Clear();
+
+        }
+    }
+}
